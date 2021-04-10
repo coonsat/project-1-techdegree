5e8d847781fea9a1a6ebcd48cd7ce6c7dfe01222
coonsat <32741582+coonsat@users.noreply.github.com>
2021-04-10 12:34:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+const quotes = 
+[
+    {
+        "quote" : "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel",
+        "source" : "Maya Angelou",
+        "citation" : null,
+        "year": null
+    },
+    {
+        "quote" : "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.",
+        "source" : "Nelson Mandela",
+        "citation" : null,
+        "year": null
+    },
+    {
+        "quote" : "Our lives begin to end the day we become silent about things that matter",
+        "source" : "Martin Luther King",
+        "citation" : null,
+        "year": null
+    },
+    {
+        "quote" : "It's not the years in your life that count. It's the life in your years",
+        "source" : "Abraham Lincoln",
+        "citation" : null,
+        "year": null
+    },
+    {
+        "quote" : "If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced",
+        "source" : "Vincent Van Gogh",
+        "citation" : "The Letters of Van Gogh",
+        "year": null
+    },
+    {
+        "quote" : "Everyone thinks of changing the world, but no one thinks of changing himself",
+        "source" : "Lev Tolstoy",
+        "citation" : null,
+        "year": 2015
+    },
+    {
+        "quote" : "Strive not to be a success, but rather to be of value",
+        "source" : "Albert Einstein",
+        "citation" : "Memoirs",
+        "year": 2017
+    },
+    {
+        "quote" : "Keep your face always towards the sunshine - and shadows will fall behind you",
+        "source" : "Walt Whitman",
+        "citation" : "Great Quotes of Our Modern Times",
+        "year": 2016
+    }
+];
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+/***
+ * `printQuote` function
+***/
+
+function printQuote() {
+  const quote = getRandomQuote();
+  let quoteBox = document.getElementById('quote-box');
+  quoteBox.innerHTML = "";
+  quoteBox.innerHTML = 
+    `<p class="quote">${quote.quote}</p>
+    <p class="source">${quote.source}${quote.citation !== null ? `<span class=citation>${quote.citation}</span>` : ''}${quote.year !== null ? `<span class=year>${quote.year}</span>` : ''}</p>`
+}
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
